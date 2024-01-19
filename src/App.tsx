640b1f953915ecead6b89ce1ac7868f9b9796be8
Ilia Pachulia <ilia.pachulia@qauntori.com>
2024-01-19 16:38:10 +0400

    added file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-     <h1 className='bg-red-400'>tailwind?</h1>
+      <h1 className='font-semibold text-yellow-300 rounded-lg bg-red-400 flex items-center justify-center'>
+        Tailwind?
+      </h1>
     </>
   );
 }
