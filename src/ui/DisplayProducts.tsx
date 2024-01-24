9ee2cd4d327ff71b94b3e0e18257e8ae40897315
Ilia Pachulia <ilia.pachulia@qauntori.com>
2024-01-24 20:01:34 +0400

    added pagination logic

--- src/ui/DisplayProducts.tsx
+++ src/ui/DisplayProducts.tsx
@@ -10,10 +10,14 @@ import Spinner from '../ui/Spinner';
 import Error from '../ui/Error';
 import { CardProps, ProductsProps } from '../utils/interfaces';
 import { CategoriesContext } from '../context/CategoriesContext';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import Button from './Button';
+import { PAGINATION_CHUNK_SIZE } from '../utils/constants';
 
 function DisplayProducts({ products }: ProductsProps) {
   const { selectedCategories } = useContext(CategoriesContext);
-  const [priceRange, setPriceRange] = useState<number[]>([0, 500]);
+  const [priceRange, setPriceRange] = useState<number[]>([0, 1500]);
+  const [page, setPage] = useState(1);
 
   const { isLoading, data, error } = useQuery({
     queryKey: ['categories'],
@@ -27,6 +31,12 @@ function DisplayProducts({ products }: ProductsProps) {
     ...(data || []),
   ];
 
+  const totalPages = Math.ceil((products?.length || 0) / PAGINATION_CHUNK_SIZE);
+
+  const endIndex = page * PAGINATION_CHUNK_SIZE;
+  const startIndex = endIndex - PAGINATION_CHUNK_SIZE;
+  const currentPageProducts = products?.slice(startIndex, endIndex);
+
   const filteredProductsQuery = useQuery({
     queryKey: ['filteredProducts', selectedCategories],
     queryFn: () => getFilteredByCategories(selectedCategories),
@@ -37,11 +47,21 @@ function DisplayProducts({ products }: ProductsProps) {
     filteredProductsQuery;
 
   const filteredProducts = (
-    filteredByCategoryNames ? filteredByCategoryNames : products
+    filteredByCategoryNames ? filteredByCategoryNames : currentPageProducts
   ).filter(
     (product: CardProps) =>
       product.price >= priceRange[0] && product.price <= priceRange[1]
   ) as CardProps[];
+
+  const nextPage = () => {
+    if (page < totalPages) setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) setPage(page - 1);
+  };
+
+  const isLastPage = page >= totalPages;
 
   if (isLoading) return <Spinner />;
   if (error) return <Error message={error as string} />;
@@ -56,8 +76,52 @@ function DisplayProducts({ products }: ProductsProps) {
           priceRange={priceRange}
         />
       </div>
-      <div className='w-[896px]'>
+      <div className='w-[896px] flex flex-col'>
         <ProductList products={[...filteredProducts]} />
+        <nav className='flex justify-end items-center self-end w-40 my-20'>
+          <button
+            disabled={page === 1}
+            className={
+              page === 1
+                ? 'bg-gray-200 px-1 rounded mx-2 focus:cursor-pointer'
+                : ''
+            }
+            onClick={prevPage}
+          >
+            <LeftOutlined />
+          </button>
+          <button
+            className={
+              page - 1 === 0
+                ? 'hidden'
+                : 'px-2 rounded border mx-2 focus:cursor-pointer'
+            }
+            onClick={prevPage}
+          >
+            {page - 1}
+          </button>
+          <button className='border border-[#374151] px-2 rounded mx-2 focus:cursor-pointer'>
+            {page}
+          </button>
+          <button
+            className={`border px-2 rounded mx-2 focus:cursor-pointer ${
+              isLastPage ? 'hidden' : ''
+            }`}
+            onClick={nextPage}
+            disabled={isLastPage}
+          >
+            {page + 1}
+          </button>
+          <button
+            className={`border px-1 rounded mx-2 focus:cursor-pointer ${
+              isLastPage ? 'bg-gray-200' : ''
+            }`}
+            onClick={nextPage}
+            disabled={isLastPage}
+          >
+            <RightOutlined />
+          </button>
+        </nav>
       </div>
     </div>
   );
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const API_URL = "https://fakestoreapi.com";
 export const READ_MORE_CHUNK_SIZE = 5;
+export const PAGINATION_CHUNK_SIZE = 6;
